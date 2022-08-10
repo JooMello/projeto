d8f7fe3b09a49440db94ef77f7774e4bf6c64175
PaoQueimado <jvssmello@gmail.com>
2022-08-10 11:14:11 -0400

    FilterDataAno

--- payments/PaymentsController.js
+++ payments/PaymentsController.js
@@ -17,7 +17,6 @@ const {
 
 
 //lista todos os pagamento
-
 router.get("/admin/payments", adminAuth, async (req, res, next) => {
 
   var totalPayments1 = 0;
@@ -180,7 +179,7 @@ router.get("/admin/payments", adminAuth, async (req, res, next) => {
       model: Category,
     }, ],
     order: [
-      ["id", "DESC"]
+      ["data", "DESC"]
     ],
     limit: 20,
    raw: true,
@@ -321,7 +320,7 @@ router.get("/payments/page/:num", (req, res) => {
       model: Category,
     }, ],
     order: [
-      ["id", "DESC"]
+      ["data", "DESC"]
     ],
     limit: 20,
     offset: offset,
@@ -344,6 +343,589 @@ router.get("/payments/page/:num", (req, res) => {
         result: result,
         payments: payments,
         categories: categories,
+      });
+    });
+  })
+});
+
+router.get("/admin/filter/2021", adminAuth, async (req, res, next) => {
+
+  var totalPayments1 = 0;
+  var totalPayments2 = 0;
+  var totalPayments3 = 0;
+  var totalPayments4 = 0;
+  var totalPayments5 = 0;
+  var totalPayments6 = 0;
+  var totalPayments7 = 0;
+  var totalPayments8 = 0;
+  var totalPayments9 = 0;
+  var totalPayments10 = 0;
+  var totalPayments11 = 0;
+  var totalPayments12 = 0;
+  var totalPayments13 = 0;
+  var totalPayments14 = 0;
+
+  var amount1 = await Payment.findOne({
+    where: {
+      categoryId: 1,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments1 = amount1['sum(`valor`)']
+
+  var amount2 = await Payment.findOne({
+    where: {
+      categoryId: 2,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+
+  var totalPayments3 = amount2['sum(`valor`)']
+  var amount3 = await Payment.findOne({
+    where: {
+      categoryId: 3,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments3 = amount3['sum(`valor`)']
+
+  var amount4 = await Payment.findOne({
+    where: {
+      categoryId: 4,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments2 = amount4['sum(`valor`)']
+
+
+  var amount5 = await Payment.findOne({
+    where: {
+      categoryId: 5,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments5 = amount5['sum(`valor`)']
+
+  var amount6 = await Payment.findOne({
+    where: {
+      categoryId: 6,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments6 = amount6['sum(`valor`)']
+
+
+
+  var amount7 = await Payment.findOne({
+    where: {
+      categoryId: 7,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments7 = amount7['sum(`valor`)']
+
+
+  var amount8 = await Payment.findOne({
+    where: {
+      categoryId: 8,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments8 = amount8['sum(`valor`)']
+
+
+
+  var amount9 = await Payment.findOne({
+    where: {
+      categoryId: 9,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments9 = amount9['sum(`valor`)']
+
+  var amount10 = await Payment.findOne({
+    where: {
+      categoryId: 10,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments10 = amount10['sum(`valor`)']
+
+
+  var amount11 = await Payment.findOne({
+    where: {
+      categoryId: 11,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments11 = amount11['sum(`valor`)']
+
+
+  var amount12 = await Payment.findOne({
+    where: {
+      categoryId: 12,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments12 = amount12['sum(`valor`)']
+
+
+
+  var amount13 = await Payment.findOne({
+    where: {
+      categoryId: 13,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments13 = amount13['sum(`valor`)']
+
+
+  var amount14 = await Payment.findOne({
+    where: {
+      categoryId: 14,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments14 = amount14['sum(`valor`)']
+
+
+
+  Payment.findAll({
+    include: [{
+      model: Category,
+    }, ],
+    order: [
+      ["data", "DESC"]
+    ],
+    limit: 20,
+    where: {
+      data: {
+        [Op.between]: [new Date("Jan 01 2021"), new Date("Dec 31 2021")],
+      },
+    },
+
+  }).then((payments) => {
+
+    Category.findAll().then((categories) => {
+      res.render("admin/payments/index", {
+        payments: payments,
+        categories: categories,
+        totalPayments1: totalPayments1,
+        totalPayments2: totalPayments2,
+        totalPayments3: totalPayments3,
+        totalPayments4: totalPayments4,
+        totalPayments5: totalPayments5,
+        totalPayments6: totalPayments6,
+        totalPayments7: totalPayments7,
+        totalPayments8: totalPayments8,
+        totalPayments9: totalPayments9,
+        totalPayments10: totalPayments10,
+        totalPayments11: totalPayments11,
+        totalPayments12: totalPayments12,
+        totalPayments13: totalPayments13,
+        totalPayments14: totalPayments14,
+      });
+    });
+  })
+});
+router.get("/admin/filter/2022", adminAuth, async (req, res, next) => {
+
+  var totalPayments1 = 0;
+  var totalPayments2 = 0;
+  var totalPayments3 = 0;
+  var totalPayments4 = 0;
+  var totalPayments5 = 0;
+  var totalPayments6 = 0;
+  var totalPayments7 = 0;
+  var totalPayments8 = 0;
+  var totalPayments9 = 0;
+  var totalPayments10 = 0;
+  var totalPayments11 = 0;
+  var totalPayments12 = 0;
+  var totalPayments13 = 0;
+  var totalPayments14 = 0;
+
+  var amount1 = await Payment.findOne({
+    where: {
+      categoryId: 1,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments1 = amount1['sum(`valor`)']
+
+  var amount2 = await Payment.findOne({
+    where: {
+      categoryId: 2,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+
+  var totalPayments3 = amount2['sum(`valor`)']
+  var amount3 = await Payment.findOne({
+    where: {
+      categoryId: 3,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments3 = amount3['sum(`valor`)']
+
+  var amount4 = await Payment.findOne({
+    where: {
+      categoryId: 4,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments2 = amount4['sum(`valor`)']
+
+
+  var amount5 = await Payment.findOne({
+    where: {
+      categoryId: 5,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments5 = amount5['sum(`valor`)']
+
+  var amount6 = await Payment.findOne({
+    where: {
+      categoryId: 6,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments6 = amount6['sum(`valor`)']
+
+
+
+  var amount7 = await Payment.findOne({
+    where: {
+      categoryId: 7,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments7 = amount7['sum(`valor`)']
+
+
+  var amount8 = await Payment.findOne({
+    where: {
+      categoryId: 8,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments8 = amount8['sum(`valor`)']
+
+
+
+  var amount9 = await Payment.findOne({
+    where: {
+      categoryId: 9,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments9 = amount9['sum(`valor`)']
+
+  var amount10 = await Payment.findOne({
+    where: {
+      categoryId: 10,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments10 = amount10['sum(`valor`)']
+
+
+  var amount11 = await Payment.findOne({
+    where: {
+      categoryId: 11,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments11 = amount11['sum(`valor`)']
+
+
+  var amount12 = await Payment.findOne({
+    where: {
+      categoryId: 12,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments12 = amount12['sum(`valor`)']
+
+
+
+  var amount13 = await Payment.findOne({
+    where: {
+      categoryId: 13,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments13 = amount13['sum(`valor`)']
+
+
+  var amount14 = await Payment.findOne({
+    where: {
+      categoryId: 14,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments14 = amount14['sum(`valor`)']
+
+
+
+  Payment.findAll({
+    include: [{
+      model: Category,
+    }, ],
+    order: [
+      ["data", "DESC"]
+    ],
+    limit: 20,
+    where: {
+      data: {
+        [Op.between]: [new Date("Jan 01 2022"), new Date("Dec 31 2022")],
+      },
+    },
+
+  }).then((payments) => {
+
+    Category.findAll().then((categories) => {
+      res.render("admin/payments/index", {
+        payments: payments,
+        categories: categories,
+        totalPayments1: totalPayments1,
+        totalPayments2: totalPayments2,
+        totalPayments3: totalPayments3,
+        totalPayments4: totalPayments4,
+        totalPayments5: totalPayments5,
+        totalPayments6: totalPayments6,
+        totalPayments7: totalPayments7,
+        totalPayments8: totalPayments8,
+        totalPayments9: totalPayments9,
+        totalPayments10: totalPayments10,
+        totalPayments11: totalPayments11,
+        totalPayments12: totalPayments12,
+        totalPayments13: totalPayments13,
+        totalPayments14: totalPayments14,
+      });
+    });
+  })
+});
+router.get("/admin/filter/2023", adminAuth, async (req, res, next) => {
+
+  var totalPayments1 = 0;
+  var totalPayments2 = 0;
+  var totalPayments3 = 0;
+  var totalPayments4 = 0;
+  var totalPayments5 = 0;
+  var totalPayments6 = 0;
+  var totalPayments7 = 0;
+  var totalPayments8 = 0;
+  var totalPayments9 = 0;
+  var totalPayments10 = 0;
+  var totalPayments11 = 0;
+  var totalPayments12 = 0;
+  var totalPayments13 = 0;
+  var totalPayments14 = 0;
+
+  var amount1 = await Payment.findOne({
+    where: {
+      categoryId: 1,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments1 = amount1['sum(`valor`)']
+
+  var amount2 = await Payment.findOne({
+    where: {
+      categoryId: 2,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+
+  var totalPayments3 = amount2['sum(`valor`)']
+  var amount3 = await Payment.findOne({
+    where: {
+      categoryId: 3,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments3 = amount3['sum(`valor`)']
+
+  var amount4 = await Payment.findOne({
+    where: {
+      categoryId: 4,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments2 = amount4['sum(`valor`)']
+
+
+  var amount5 = await Payment.findOne({
+    where: {
+      categoryId: 5,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments5 = amount5['sum(`valor`)']
+
+  var amount6 = await Payment.findOne({
+    where: {
+      categoryId: 6,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments6 = amount6['sum(`valor`)']
+
+
+
+  var amount7 = await Payment.findOne({
+    where: {
+      categoryId: 7,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments7 = amount7['sum(`valor`)']
+
+
+  var amount8 = await Payment.findOne({
+    where: {
+      categoryId: 8,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments8 = amount8['sum(`valor`)']
+
+
+
+  var amount9 = await Payment.findOne({
+    where: {
+      categoryId: 9,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments9 = amount9['sum(`valor`)']
+
+  var amount10 = await Payment.findOne({
+    where: {
+      categoryId: 10,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments10 = amount10['sum(`valor`)']
+
+
+  var amount11 = await Payment.findOne({
+    where: {
+      categoryId: 11,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments11 = amount11['sum(`valor`)']
+
+
+  var amount12 = await Payment.findOne({
+    where: {
+      categoryId: 12,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments12 = amount12['sum(`valor`)']
+
+
+
+  var amount13 = await Payment.findOne({
+    where: {
+      categoryId: 13,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments13 = amount13['sum(`valor`)']
+
+
+  var amount14 = await Payment.findOne({
+    where: {
+      categoryId: 14,
+    },
+    attributes: [sequelize.fn("sum", sequelize.col("valor"))],
+    raw: true
+  });
+  var totalPayments14 = amount14['sum(`valor`)']
+
+
+
+  Payment.findAll({
+    include: [{
+      model: Category,
+    }, ],
+    order: [
+      ["data", "DESC"]
+    ],
+    limit: 20,
+    where: {
+      data: {
+        [Op.between]: [new Date("Jan 01 2023"), new Date("Dec 31 2023")],
+      },
+    },
+
+  }).then((payments) => {
+
+    Category.findAll().then((categories) => {
+      res.render("admin/payments/index", {
+        payments: payments,
+        categories: categories,
+        totalPayments1: totalPayments1,
+        totalPayments2: totalPayments2,
+        totalPayments3: totalPayments3,
+        totalPayments4: totalPayments4,
+        totalPayments5: totalPayments5,
+        totalPayments6: totalPayments6,
+        totalPayments7: totalPayments7,
+        totalPayments8: totalPayments8,
+        totalPayments9: totalPayments9,
+        totalPayments10: totalPayments10,
+        totalPayments11: totalPayments11,
+        totalPayments12: totalPayments12,
+        totalPayments13: totalPayments13,
+        totalPayments14: totalPayments14,
       });
     });
   })
@@ -510,7 +1092,7 @@ router.get("/admin/filter/janeiro", adminAuth, async (req, res, next) => {
       model: Category,
     }, ],
     order: [
-      ["id", "DESC"]
+      ["data", "DESC"]
     ],
     limit: 20,
     where: {
@@ -704,7 +1286,7 @@ router.get("/admin/filter/fevereiro", adminAuth, async (req, res, next) => {
       model: Category,
     }, ],
     order: [
-      ["id", "DESC"]
+      ["data", "DESC"]
     ],
     limit: 20,
     where: {
@@ -898,7 +1480,7 @@ router.get("/admin/filter/marco", adminAuth, async (req, res, next) => {
       model: Category,
     }, ],
     order: [
-      ["id", "DESC"]
+      ["data", "DESC"]
     ],
     limit: 20,
     where: {
@@ -1092,7 +1674,7 @@ router.get("/admin/filter/abril", adminAuth, async (req, res, next) => {
       model: Category,
     }, ],
     order: [
-      ["id", "DESC"]
+      ["data", "DESC"]
     ],
     limit: 20,
     where: {
@@ -1286,7 +1868,7 @@ router.get("/admin/filter/maio", adminAuth, async (req, res, next) => {
       model: Category,
     }, ],
     order: [
-      ["id", "DESC"]
+      ["data", "DESC"]
     ],
     limit: 20,
     where: {
@@ -1480,7 +2062,7 @@ router.get("/admin/filter/junho", adminAuth, async (req, res, next) => {
       model: Category,
     }, ],
     order: [
-      ["id", "DESC"]
+      ["data", "DESC"]
     ],
     limit: 20,
     where: {
@@ -1674,7 +2256,7 @@ router.get("/admin/filter/julho", adminAuth, async (req, res, next) => {
       model: Category,
     }, ],
     order: [
-      ["id", "DESC"]
+      ["data", "DESC"]
     ],
     limit: 20,
     where: {
@@ -1868,7 +2450,7 @@ router.get("/admin/filter/agosto", adminAuth, async (req, res, next) => {
       model: Category,
     }, ],
     order: [
-      ["id", "DESC"]
+      ["data", "DESC"]
     ],
     limit: 20,
     where: {
@@ -2062,7 +2644,7 @@ router.get("/admin/filter/setembro", adminAuth, async (req, res, next) => {
       model: Category,
     }, ],
     order: [
-      ["id", "DESC"]
+      ["data", "DESC"]
     ],
     limit: 20,
     where: {
@@ -2256,7 +2838,7 @@ router.get("/admin/filter/outubro", adminAuth, async (req, res, next) => {
       model: Category,
     }, ],
     order: [
-      ["id", "DESC"]
+      ["data", "DESC"]
     ],
     limit: 20,
     where: {
@@ -2450,7 +3032,7 @@ router.get("/admin/filter/novembro", adminAuth, async (req, res, next) => {
       model: Category,
     }, ],
     order: [
-      ["id", "DESC"]
+      ["data", "DESC"]
     ],
     limit: 20,
     where: {
@@ -2644,7 +3226,7 @@ router.get("/admin/filter/dezembro", adminAuth, async (req, res, next) => {
       model: Category,
     }, ],
     order: [
-      ["id", "DESC"]
+      ["data", "DESC"]
     ],
     limit: 20,
     where: {
